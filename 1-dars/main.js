9cb43828cc6d3a62026e6ddbadbec8be291a12f4
Shaxzod <shaxzod20000823@gmail.com>
2021-06-14 13:39:00 +0500

    Javascrip tutorial

---
+++ 1-dars/main.js
@@ -0,0 +1,35 @@
+
+    function btn1(div1,div2){
+        let div=document.querySelector(div1),
+        div0=document.querySelector(div2);
+        div.style.display= "none";
+        div0.style.display= "block";
+   
+    
+    }
+
+    function btn(){
+        let btn=document.querySelector("#btn"),
+        btn10=document.querySelector("#btn10")
+        con1=document.querySelector(".content1"),
+        con=document.querySelector(".content");
+        btn.style.display="none";
+        btn10.style.display="block";
+        con1.style.display="block";
+        con.style.display="none";
+
+
+    }
+    function btn10(){
+        let btn=document.querySelector("#btn"),
+        btn10=document.querySelector("#btn10")
+        con1=document.querySelector(".content1"),
+        con=document.querySelector(".content");
+        btn.style.display="block";
+        btn10.style.display="none";
+        con1.style.display="none";
+        con.style.display="block";
+
+    }
+
+
